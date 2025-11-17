d3d67cfb373209ec0ab1b7d5c537f0c62df7a181
FelipeLMoreir <felipe.lm.moreira@gmail.com>
2025-11-17 14:42:59 -0300

    select

--- ProjADONET/Program.cs
+++ ProjADONET/Program.cs
@@ -3,11 +3,39 @@ using ProjADONET;
 
 var connection = new SqlConnection(DBConnection.GetConnectionString());
 
-var pessoa = new Pessoa("João Silva", "123.456.789-00", new DateOnly(1990, 5, 15));
+var pessoa = new Pessoa("João Silva", "12345678900", new DateOnly(1990, 5, 15));
 
-
+var sqlInsertPessoa = $"INSERT INTO Pessoas (nome, cpf, dataNascimento) " +
+    $"VALUES (@Nome, @CPF, @DataNascimento)";
 
 connection.Open();
+
+var command = new SqlCommand(sqlInsertPessoa, connection);
+
+command.Parameters.AddWithValue("@Nome", pessoa.Nome);
+command.Parameters.AddWithValue("@CPF", pessoa.CPF);
+command.Parameters.AddWithValue("@DataNascimento", pessoa.DataNascimento);
+
+int linhas = command.ExecuteNonQuery();
+
+if (linhas > 0)
+{
+    Console.WriteLine("Pessoa Inserida com sucesso!");
+}
+else
+{
+    Console.WriteLine("Erro ao inserir pessoa!");
+}
+
+connection.Close();
+
+connection.Open();
+
+var sqlSelectPessoas = "SELECT id, nome, cpf, dataNascimento FROM Pessoas";
+
+command = new SqlCommand(sqlSelectPessoas, connection);
+
+var reader = command.ExecuteReader();
 
 
 
